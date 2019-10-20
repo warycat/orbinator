6dc17488ba96804e9eb49d0de9e01bad94a0ee4c
YINCHU XIA <larry.fantasy@gmail.com>
2019-10-20 01:37:06 -0700

    all data files

--- src/index.js
+++ src/index.js
@@ -4,15 +4,80 @@ import _ from 'lodash';
 import Button from '@material-ui/core/Button';
 import * as THREE from 'three';
 import stations_txt from './stations.txt';
-
+import visual_txt from './visual.txt';
+import active_txt from './active.txt';
+import analyst_txt from './analyst.txt';
+import _2019_006_txt from './2019-006.txt';
+import _1999_025_txt from './1999-025.txt';
+import iridium_33_debris_txt from './iridium-33-debris.txt';
+import cosmos_2251_debris_txt from  './cosmos-2251-debris.txt';
+import _2012_044_txt from './2012-044.txt';
 var satellite = require('satellite.js');
 
+var set = [
+  {
+    name: "Space Stations",
+    data: stations_txt,
+  },
+  {
+    name: "100 (or so) Brightest",
+    data: visual_txt,
+  },
+  {
+    name: "Analyst Satellites",
+    data: analyst_txt,
+  },
+  {
+    name: "Indian ASAT Test Debris",
+    data: _2019_006_txt,
+  },
+  {
+    name: "FENGYUN 1C Debris",
+    data: _1999_025_txt,
+  },
+  {
+    name: "IRIDIUM 33 Debris",
+    data: iridium_33_debris_txt,
+  },
+  {
+    name: "COSMOS 2251 Debris",
+    data: cosmos_2251_debris_txt,
+  },
+  {
+    name: "BREEZE-M R/B Breakup (2012-044C)",
+    data: _2012_044_txt,
+  }
+]
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+
+var seq = getUrlParameter("seq") || Math.floor(Math.random() * 8);
+var pick = set[seq];
+document.getElementById("info").innerHTML = pick.name;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var stations = [];
+var scene = new THREE.Scene();
+
+var geometry = new THREE.SphereBufferGeometry( 2.6, 30, 30 );
+var ocean_material = new THREE.MeshLambertMaterial({
+  color: 0x00ff00,
+  emissive: 0x0000ff,
+  transparent: true,
+  opacity: 0.8,
+});
+
+var earth = new THREE.Mesh( geometry, ocean_material );
+scene.add( earth );
+
 
 function parse_lines(txt){
   var lines = txt.split('\n');
@@ -49,94 +114,61 @@ function Orbinator(name, line2, line3){
   this.no = satrec.no;
 }
 
-let obinators = parse_lines(stations_txt);
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-console.log(obinators);
+let obinators = parse_lines(pick.data);
 
-// Sample TLE
-var tleLine1 = '1 25544U 98067A   19156.50900463  .00003075  00000-0  59442-4 0  9992',
-    tleLine2 = '2 25544  51.6433  59.2583 0008217  16.4489 347.6017 15.51174618173442';    
+for (var o of obinators) {
+  var p = 0.4 / o.no;
+  var c = Math.pow(p * p, 1 / 3.0);
+  var a = c * (1 + o.ecco);
+  var b = c * (1 - o.ecco);
+  console.log(a, b);
 
-// Initialize a satellite record
-var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
+  var curve = new THREE.EllipseCurve(
+    c * o.ecco,  0,            // ax, aY
+    a, b,           // xRadius, yRadius
+    0,  2 * Math.PI,  // aStartAngle, aEndAngle
+    false,            // aClockwise
+    0                 // aRotation
+  );
+  
+  var points = curve.getPoints( 50 );
+  var geometry = new THREE.BufferGeometry().setFromPoints( points );
+  
+  var material = new THREE.LineBasicMaterial( { color : (1<<24)*Math.random() } );
+  
+  // Create the final object to add to the scene
+  var ellipse = new THREE.Line( geometry, material );
+  ellipse.rotation.z = o.argpo;
+  ellipse.rotation.x = o.nodeo;
+  ellipse.rotation.y = o.inclo;
 
-console.log('satrec.inclo', satrec.inclo);
-console.log('satrec.nodeo', satrec.nodeo);
-console.log('satrec.ecco', satrec.ecco);
-console.log('satrec.argpo', satrec.argpo);
-console.log('satrec.mo', satrec.mo);
-console.log('satrec.no', satrec.no);
-
-// inclo Inclination in radians.
-// nodeo Right ascension of ascending node in radians.
-// ecco Eccentricity.
-// argpo Argument of perigee in radians.
-// mo Mean anomaly in radians.
-// no Mean motion in radians per minute.
+  scene.add(ellipse);
+}
 
 
-var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-
-var geometry = new THREE.SphereBufferGeometry( 1, 20, 20 );
-var ocean_material = new THREE.MeshLambertMaterial({
-  color: 0x0000ff,
-  emissive: 0x0000ff,
-  transparent: true,
-  opacity: 0.5,
-  wireframe: true
-});
-
-var cube = new THREE.Mesh( geometry, ocean_material );
-scene.add( cube );
-
-var curve = new THREE.EllipseCurve(
-	0,  0,            // ax, aY
-	2, 2.5,           // xRadius, yRadius
-	0,  2 * Math.PI,  // aStartAngle, aEndAngle
-	false,            // aClockwise
-	1                 // aRotation
-);
-
-var points = curve.getPoints( 50 );
-var geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-
-// Create the final object to add to the scene
-var ellipse = new THREE.Line( geometry, material );
-ellipse.rotation.x = Math.PI / 2;
-ellipse.rotation.y = Math.PI / 4;
-
-scene.add(ellipse);
-
-camera.position.y = 2;
-
 function animate() {
-  // cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
 
-  var speed = Date.now() * 0.0001;
-  camera.position.x = Math.cos(speed) * 5;
-  camera.position.z = Math.sin(speed) * 5;
+  var speed = Date.now() * 0.0002;
+  camera.position.x = Math.cos(speed) * (15 + 10 * Math.cos(0.5 * speed));
+  camera.position.z = Math.sin(speed) * (15 + 10 * Math.cos(0.5 * speed));
 
   camera.lookAt(scene.position); //0,0,0
 
   requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
+
+
 animate();
 
-// function App() {
-//   return (
-//     <Button variant="contained" color="primary">
-//       Hello World
-//     </Button>
-//   );
-// }
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
